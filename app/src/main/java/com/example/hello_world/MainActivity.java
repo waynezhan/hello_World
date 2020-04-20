748b6f570845da9c46d8aaa439c18561388f6301
waynezhan <33579680@qq.com>
2020-04-20 19:57:48 +0800

    2020-4-20 19:57:29

--- app/src/main/java/com/example/hello_world/MainActivity.java
+++ app/src/main/java/com/example/hello_world/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.hello_world;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mBtnTextview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnTextview = findViewById(R.id.btn_textview1);
+        mBtnTextview.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //jump text1 page
+                Intent intent = new Intent(MainActivity.this,text_viewActivity.class);
+                startActivity (intent);
+
+
+            }
+
+        });
     }
 }
